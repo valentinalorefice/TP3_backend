b3281b758895e682d21d4bafec1256b0fb7072c6
valentinalorefice <163413854+valentinalorefice@users.noreply.github.com>
2025-05-26 18:44:35 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,18 +1,30 @@
+using ArticulosAPI;
 using ArticulosAPI.Data;
+using ArticulosAPI.Repositorio; // <--- NUEVO
+using AutoMapper;                // <--- NUEVO
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Conexión a la base de datos
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+// AutoMapper
+var mapperConfig = MappingConfig.RegisterMaps(); // <--- NUEVO
+IMapper mapper = mapperConfig.CreateMapper();    // <--- NUEVO
+builder.Services.AddSingleton(mapper);           // <--- NUEVO
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies()); // <--- NUEVO
+
+// Repositorio
+builder.Services.AddScoped<IArticuloRepositorio, ArticuloRepositorio>(); // <--- NUEVO
 
 var app = builder.Build();
 
@@ -24,9 +36,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
